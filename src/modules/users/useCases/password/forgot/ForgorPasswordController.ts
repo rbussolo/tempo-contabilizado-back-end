c309732d9de491d3027ccec28c974a0ed4c840e9
Rodrigo Bussolo <rbussolo91@gmail.com>
2023-06-05 09:31:28 -0400

    Correção do procedimento de esqueci a senha, estava exigindo o CPF/CNPJ da pessoa.

--- src/modules/users/useCases/password/forgot/ForgorPasswordController.ts
+++ src/modules/users/useCases/password/forgot/ForgorPasswordController.ts
@@ -5,10 +5,10 @@ import { removeMaskCpfCnpj } from '../../../../../utils/RemoveMaskCpfCnpj';
 
 export class ForgotPasswordController {
   async handle(request: Request, response: Response){
-    const { email, cpf_cnpj } = request.body;
+    const { email } = request.body;
 
     const service = new ForgotPasswordService();
-    const result = await service.execute({ email, cpf_cnpj: removeMaskCpfCnpj(cpf_cnpj) });
+    const result = await service.execute({ email });
 
     if(result instanceof AppError) {
       return response.status(result.statusCode).json({ message: result.message });
--- src/modules/users/useCases/password/forgot/ForgotPasswordService.ts
+++ src/modules/users/useCases/password/forgot/ForgotPasswordService.ts
@@ -1,24 +1,21 @@
 import { AppDataSource } from "../../../../../data-source";
 import { AppError } from "../../../../../errors/AppError";
 import { Auth } from "../../../../auth/Auth";
-import { Smtp } from "../../../../emails/smtp";
+import { Smtp } from "../../../../emails/Smtp";
 import { User } from "../../../entities/User";
 
 interface ForgotPassword {
   email: string;
-  cpf_cnpj: string;
 }
 
 export class ForgotPasswordService {
-  async execute({ email, cpf_cnpj }: ForgotPassword): Promise<AppError> {
+  async execute({ email }: ForgotPassword): Promise<AppError> {
     if (!email) {
       return new AppError("É necessário informar o E-mail!");
-    } else if(!cpf_cnpj) {
-      return new AppError("É necessário informar o CPF/CNPJ!");
     }
 
     const repo = AppDataSource.getRepository(User);
-    const user = await repo.findOne({ where: { email, cpf_cnpj }});
+    const user = await repo.findOne({ where: { email }});
 
     if (!user) {
       return new AppError("Não foi localizado nenhuma conta com os dados informados!");
